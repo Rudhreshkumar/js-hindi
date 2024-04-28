ef39175042d4413591d29e4fffda376b1b3562d4
Rudhreshkumar <155624302+Rudhreshkumar@users.noreply.github.com>
2024-04-28 14:49:11 +0000

    added a varaible without keyword

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,5 @@
-var acc=1122;
-let em="hitesh@gmail.com";
+var acc=1122;//global scope
+let em="hitesh@gmail.com"; // local scope
+something = "jaipur"
 
-console.table([acc,em]);
+console.table([acc,em,something]);
