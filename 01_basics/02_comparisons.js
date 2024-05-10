c8a0dfb41e3231c8b38a82406e612685a06f7fe7
Rudhreshkumar <155624302+Rudhreshkumar@users.noreply.github.com>
2024-05-10 10:21:51 +0000

    comp updated

--- 01_basics/02_comparisons.js
+++ 01_basics/02_comparisons.js
@@ -1,3 +1,3 @@
 console.log(null>0) //false
 console.log(null == 0) //false
-console.log(null>=0) //true
+console.log(null>=0) //true here null ,is converted into a number '0' comparison is different from equality check    --- >= is different from > and different from ==,===
