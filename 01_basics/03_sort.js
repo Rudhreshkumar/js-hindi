93938ce79734e76074805bc98faa4e22f9a17c0c
Rudhreshkumar <155624302+Rudhreshkumar@users.noreply.github.com>
2024-05-17 14:28:41 +0000

    trail commit

---
+++ 01_basics/03_sort.js
@@ -0,0 +1,4 @@
+const nums =["1","2"]
+const x=Number(nums[0])
+
+console.log(typeof x)
--- 01_basics/Revision_datatypes.js
+++ 01_basics/Revision_datatypes.js
@@ -2,3 +2,6 @@
 // 7 types  :String,boolean,number,null,undefined
 // symbol is used to make something unique
 // non primitive  and reference type  
+
+
+
